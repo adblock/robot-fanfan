3a39e03c6c8ed2527470d4b0e2b981bf79b487ad
wangyuanshuai <wangyuanshuai@jupin.net.cn>
2021-05-13 17:17:13 +0800

    添加注释

--- src/tuijian/adjuster/adjuster.interface.ts
+++ src/tuijian/adjuster/adjuster.interface.ts
@@ -1,10 +1,8 @@
 /**
  * Adjuster 类接口 构造API请求的必须数据
  * 
- * api  淘宝API的名称，发送请求的必须字段
- * data 发送的接口数据，接口需要的数据
- * */
+ **/
 export interface AdjusterInterface {
-    api:string;
-    data:any;
+    api:string; // 淘宝API的名称，发送请求的必须字段
+    data:any;   // 发送的接口数据，接口需要的数据
 }

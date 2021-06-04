e7450a5936cf4d98c0c4cc1939f3f5778feceb3e
wangyuanshuai <wangyuanshuai@jupin.net.cn>
2021-06-04 16:23:39 +0800

    修改mongo

--- src/jushita/models/token.ts
+++ src/jushita/models/token.ts
@@ -1,5 +1,9 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
-const Token = new Schema({}, { strict: false });
+const Token = new Schema({
+    created_at: { type: Date, default: Date.now },
+    update_at: { type: Date, default: Date.now }
+    }, { strict: false }
+);
 const TokenModel = mongoose.model('jushita_tokens', Token);
 export { TokenModel };

ce97fda733129ec02321bd75aee4ba449c2b1216
wangyuanshuai <wangyuanshuai@jupin.net.cn>
2021-06-07 10:45:43 +0800

    将所有请求改为超级女生琳琳的账户

--- src/jushita/controllers/api/zhitongche.ts
+++ src/jushita/controllers/api/zhitongche.ts
@@ -6,7 +6,8 @@ const zhitongcheClient  = new ApiClient( { app_key:ZhitongcheConfig.app_key, app
 // 直通车api接口默认方法
 const index = function (req:Request, res:Response) {
     const data = req.body;
-    const result = zhitongcheClient.execute(data.method,data.params);
+    data.session = '6202806718a2bbdfc4f61fd525e4b0c58506e7709225de026151499';
+    const result = zhitongcheClient.execute(data.method, data.params, data.session);
     result.then(function (data:any) {
         res.json(data);
     });

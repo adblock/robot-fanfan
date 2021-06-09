4302ff5cc34e052997e191dc8b631cee2222559e
wangyuanshuai <wangyuanshuai@jupin.net.cn>
2021-06-09 14:28:27 +0800

    将所有请求改为超级女生琳琳的账户

--- src/strategy/strategy.interface.ts
+++ src/strategy/strategy.interface.ts
@@ -3,10 +3,8 @@
  * 
  * 策略类的接口，约定一个handle方法，返回一个AdjusterInterface类
  * */
-import { ApiInterface } from '../api/api.interface';
+
 export interface StrategyInterface {
     strategyData:object;
-    fliterData:any;
-    adjusteData:any;
-    handle():Promise<ApiInterface>;
+    handle():void;
 }

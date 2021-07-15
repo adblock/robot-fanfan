357ae2a1970d01b4dcd83f8809640e0d80261ac4
戴梦瑶 <daimengyao@jupin.net.cn>
2021-07-15 14:08:30 +0800

    issue: 从mongo库中获取店铺token

--- src/jushita/controllers/api/tuijian.ts
+++ src/jushita/controllers/api/tuijian.ts
@@ -1,18 +1,24 @@
-import {ApiClient} from "../../../libs/apiClient";
-import {TuijianApiConfig} from "../../../config";
-import { Request, Response} from 'express';
-const tuijianClient  = new ApiClient( { app_key:TuijianApiConfig.app_key, app_secret:TuijianApiConfig.app_secret});
+import { ApiClient } from "../../../libs/apiClient";
+import { TuijianApiConfig } from "../../../config";
+import { Request, Response } from 'express';
+import { TokenModel } from '../../models/token';
+const tuijianClient = new ApiClient({ app_key: TuijianApiConfig.app_key, app_secret: TuijianApiConfig.app_secret });
 
 // 超级推荐api接口默认方法
-const index = function (req:Request, res:Response) {
+const index = async function (req: Request, res: Response) {
     var data = req.body;
-    // todo 将请求的数据改为 超级女生琳琳token
-    data.session = '6202806718a2bbdfc4f61fd525e4b0c58506e7709225de026151499';
-    console.log(data);
-    var result = tuijianClient.execute(data.method, data.params,  data.session);
-    result.then(function (data) {
-        res.json(data);
-    });
+    var token = await TokenModel.findOne({ wangwang_id: data.nick_name }).exec();
+    if (token) {
+        data.session = token._doc.access_token;
+        console.log(data.session);
+        var result = tuijianClient.execute(data.method, data.params, data.session);
+        result.then(function (data) {
+            res.json(data);
+        });
+    } else {
+        res.json({ token_error: '店铺API未授权' });
+    }
+
 };
 
-export  { index }
+export { index }
